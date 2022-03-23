8e987697c58fd6e4d851bfae68c5c1886db4fae9
Roberto Peixinho <rhpeixinho@gmail.com>
2022-03-23 20:54:30 -0300

    Lógica do Xeque

--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -64,30 +64,38 @@ public class UI {
 		else {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 		}
+		if (chessMatch.getCheck()) {
+			if (chessMatch.getCurrentPlayer() == Color.BLACK ) {
+				System.out.println(ANSI_YELLOW + "CHECK!" + ANSI_RESET);
+			}
+			else {
+				System.out.println("CHECK!");
+			}
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_CYAN + (8 - i) + " " + ANSI_RESET);
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println(ANSI_CYAN + "  a b c d e f g h" + ANSI_RESET);
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_CYAN + (8 - i) + " " + ANSI_RESET);
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println(ANSI_CYAN + "  a b c d e f g h" + ANSI_RESET);
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
